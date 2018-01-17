9268030ce74e7b1af024dd41e266efb9e35ad6fc
gitdev1234 <gitdev@web.de>
2018-01-17 23:53:53 +0100

    changed main.cpp

--- GPS-Saviour/main.cpp
+++ GPS-Saviour/main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+/**
+ * main.cpp
+ * Purpose: main test-implementation file
+ *
+ * @author Wall.Of.Death
+ * @version 1.0
+ */
 
-using namespace std;
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
 
-int main() {
-
-    cout << "Hello World" << endl;
-}
