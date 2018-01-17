1eb24567d94ab7bf4a1f834d709e3fd3f75b9035
Gitdev42 <marcel_bode@web.de>
2018-01-18 00:17:45 +0100

    added Classes NeedyClient, GSMAgent, GPSAgent

---
+++ GPS-Saviour/src/NeedyClient.cpp
@@ -0,0 +1,32 @@
+/**
+ * TODO1.cpp
+ * Purpose: implements class TODO1
+ *
+ * @author TODO2
+ * @version 1.0
+ */
+
+#include "../include/TODO1.h"
+
+
+/* --- getters / setters --- */
+
+/**
+ * TODO1::getTODO3
+ * @brief getter for class member TODO3
+ * @return returns current value of TODO3
+ */
+bool TODO1::getTODO3() const {
+    return TODO3;
+}
+
+/**
+ * TODO1::setTODO3
+ * @brief setter for class member TODO3
+ * @param TODO3_ new value for class member TODO3
+ */
+void TODO1::setTODO3(bool TODO3_) {
+    TODO3 = TODO3_;
+}
+
+
