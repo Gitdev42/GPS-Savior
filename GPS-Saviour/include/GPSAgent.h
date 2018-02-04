d15e34eacc04a724fd101e43a8a63eef59fcc1ce
gitdev1234 <gitdev@web.de>
2018-02-04 20:05:16 +0100

    added documentation to GPSAgent.h

--- GPS-Saviour/include/GPSAgent.h
+++ GPS-Saviour/include/GPSAgent.h
@@ -2,7 +2,7 @@
  * GPSAgent.h
  * Purpose: defines class GPSAgent
  *
- * @author TODO2
+ * @author Wall.Of.Death
  * @version 1.0
  */
 
@@ -18,6 +18,17 @@
 
 using namespace std;
 
+/**
+ * @brief The GPSAgent class handles all GPS-functions within this project
+ *
+ * The GPSAgent class handles all GPS functionality for this project.
+ * This includes on one hand receiving raw data gps data, by interfacing a
+ * gps-hardware sensor.
+ * On the other hand this class implements functions for interpreting
+ * the raw gps data and converting it into a GeoData-object.
+ * Once the data is converted into a GeoData-object it is stored within a
+ * vector of GeoData-objects.
+ */
 class GPSAgent {
     public:
         /* --- constructors / destructors --- */
@@ -33,11 +44,12 @@ class GPSAgent {
 
         /* --- getters / setters --- */
         void setConnectionEstablished(bool val_);
-        bool getConnectionEstablished();
+        bool getConnectionEstablished() const;
 
 private:
     bool connectionEstablished;
     vector<GeoData> storedGeoData;
+    // for testing purposes
     TestBuffer& buffer = TestBuffer::getInstance();
 
 };
