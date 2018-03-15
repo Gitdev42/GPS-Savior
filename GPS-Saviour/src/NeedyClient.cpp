47b51520e4201107065975e4c56f9e6b4698d7bd
gitdev1234 <gitdev@web.de>
2018-03-15 22:50:25 +0100

    added new definition of NeedyClient

--- GPS-Saviour/src/NeedyClient.cpp
+++ GPS-Saviour/src/NeedyClient.cpp
@@ -13,5 +13,88 @@ void NeedyClient::init() {
 }
 
 void NeedyClient::execute() {
+    while(true) {
+
+
+        // check if initialization package is received, if so initialize client
+        checkReveivedInitialization();
+
+        if (checkReceivedRequesForStatusChange()) {
+            // process storing and sending tasks
+            processTasks();
+        } else {
+            if (getAuthIsRequired()) {
+                sendRequestForAuth();
+
+                // wait until either auth package is received or timeout is up
+                while ( (! checkTimeoutIsUp() ) && (!checkReceivedAuthPackage())) {
+                    // wait
+                }
+
+                if (! checkAuthCompleted()) {
+                    sendErrorPackage();
+                } else {
+                    saveRequestedStatus();
+                }
+            } else {
+                saveRequestedStatus();
+            }
+        }
+    }
+}
+
+
+/* --- getters / setters --- */
+
+bool NeedyClient::getAuthIsRequired() const {
+    return authIsRequired;
+}
+
+void NeedyClient::setAuthIsRequired(bool value) {
+    authIsRequired = value;
+}
+
+void NeedyClient::checkReveivedInitialization()
+{
+
+}
+
+bool NeedyClient::checkReceivedRequesForStatusChange()
+{
+
+}
+
+void NeedyClient::processTasks()
+{
+
+}
+
+void NeedyClient::sendRequestForAuth()
+{
+
+}
+
+bool NeedyClient::checkReceivedAuthPackage()
+{
+
+}
+
+bool NeedyClient::checkTimeoutIsUp()
+{
+
+}
+
+bool NeedyClient::checkAuthCompleted()
+{
+
+}
+
+void NeedyClient::sendErrorPackage()
+{
+
+}
+
+void NeedyClient::saveRequestedStatus()
+{
 
 }
