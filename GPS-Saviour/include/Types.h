c15879b0720bdcc58443f49c5493087258350374
gitdev1234 <gitdev@web.de>
2018-03-25 18:47:20 +0200

    merged types.h, second try

--- GPS-Saviour/include/Types.h
+++ GPS-Saviour/include/Types.h
@@ -14,17 +14,11 @@
 /* --- constants --- */
 
 /* --- enumerations --- */
-<<<<<<< HEAD
-enum PackageType {requestForAuth, requestData, requestLogging, sendData, sendAuth, authDenied};
+enum PackageType {requestForAuth, requestSingleData, requestSingeDataArray, requestStartContiniouslyData, requestStopContiniouslyData, requestStartLogging, requestStopLogging, sendData, sendAuth, authDenied};
 
 enum StoringStatus {storeSingleData,storeContinuously};
 
 enum SendingStatus {sendSingleData,sendSingleDataArray,sendContinuously};
-
-
-=======
-enum PackageType {requestForAuth, requestSingleData, requestSingeDataArray, requestStartContiniouslyData, requestStopContiniouslyData, requestStartLogging, requestStopLogging, sendData, sendAuth, authDenied};
->>>>>>> fd7a63a70e0f75986af6789f5ec9bff10a23ab09
 
 
 #endif // TYPES_H
