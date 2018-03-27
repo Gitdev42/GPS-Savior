1203a2257ec8269ead364c22faa74d0286d07e7e
gitdev1234 <gitdev@web.de>
2018-03-27 22:24:38 +0200

    added documentation to class Timer

--- GPS-Saviour/include/Timer.h
+++ GPS-Saviour/include/Timer.h
@@ -10,11 +10,38 @@
 #define Timer_H
 
 #include "Types.h"
+#include "time.h"
+#include "sys/time.h"
 
 using namespace std;
 
 /**
- * @brief TODO
+ * The Timer class
+ * @brief The timer class is a very simple count-down timer implementation
+ *
+ * The timer class is a very simple count-down timer implementation.
+ * It provides methods to set a timer interval and start, pause and stop the timer.
+ * The timer class is not really a unit which is continously counting down a time
+ * it is just a helper class which provides methods to save start and stop time
+ * and to check if the current time is alreay later than the stop time.
+ *
+ * PLEASE NOTE : This countdown timer implementation uses the unix time in milliseconds
+ *               (--> unix time = amount of milliseconds elapsed since the UNIX epoch (=1970))
+ *
+ * Example 1:
+ * Timer timer1;      // this creates an instance of the class timer
+ * timer1.init(1000); // this sets the interval length to 1 second
+ * timer1.start;      // this starts the timer
+ * if (timer1.checkTimerIsUp()) {...} // this checks if the stopTime is reached
+ *
+ * Example 2:
+ * Timer timer2(1500);  // this creates an instance of the class timer and sets the interval length to 1.5 seconds
+ * timer2.start;        // this starts the timer
+ * timer2.stopAndReset; // this stops the timer and resets startTime and stopTime, the interval still is 1.5 seconds
+ * timer2.start;        // this again starts the timer
+ * timer2.pause;        // this pauses the timer
+ * timer1.start;        // this again starts the timer
+ * if (timer1.checkTimerIsUp()) {...} // this checks if the stopTime is reached
  */
 class Timer {
     public:
@@ -24,13 +51,13 @@ class Timer {
         /* --- initialization / main execution functions --- */
         void init(int countDownTime_);
         void start();
-        bool checkTimerIsStarted();
+        bool checkTimerIsStarted() const;
         void pause();
         void stopAndReset();
-        bool checkTimerIsUp();
+        bool checkTimerIsUp() const;
 
         /* --- miscellaneous --- */
-        int  getCurrentTime();
+        int getCurrentTime() const;
 
         /* --- getters / setters --- */
         TimerStatus getStatus() const;
@@ -52,6 +79,10 @@ private:
         int countDownTime;
         int remainingTime;
 
+        /* --- miscellaneous --- */
+        unsigned int getTimeMs64() const;
+
+        /* --- getters / setters --- */
         int getRemainingTime() const;
         void setRemainingTime(int val_);
 
--- GPS-Saviour/src/Timer.cpp
+++ GPS-Saviour/src/Timer.cpp
@@ -56,7 +56,7 @@ void Timer::start() {
  * @brief check if the current status is "started"
  * @return returns the information if current status is "started"
  */
-bool Timer::checkTimerIsStarted() {
+bool Timer::checkTimerIsStarted() const {
     return (getStatus() == TimerStatus::started);
 }
 
@@ -95,7 +95,7 @@ void Timer::stopAndReset() {
  * @brief checks if count down timer is up
  * @return returns true if current time >= stopTime
  */
-bool Timer::checkTimerIsUp() {
+bool Timer::checkTimerIsUp() const {
     if (getStatus() == TimerStatus::started) {
         return ( getCurrentTime() >= getStopTime() );
     } else {
@@ -110,8 +110,48 @@ bool Timer::checkTimerIsUp() {
  * @brief returns current unix-time
  * @return returns current unix-time (milliseconds since 1970)
  */
-int Timer::getCurrentTime() {
+int Timer::getCurrentTime() const {
+    return getTimeMs64();
+}
 
+/**
+ * Timer::getTimeMs64
+ * Returns the amount of milliseconds elapsed since the UNIX epoch.
+ * Works on both windows and linux.
+ * @source https://stackoverflow.com/questions/1861294/how-to-calculate-execution-time-of-a-code-snippet-in-c
+ */
+unsigned int Timer::getTimeMs64() const {
+    #ifdef _WIN32
+         /* Windows */
+         FILETIME ft;
+         LARGE_INTEGER li;
+
+         /* Get the amount of 100 nano seconds intervals elapsed since January 1, 1601 (UTC) and copy it
+          * to a LARGE_INTEGER structure. */
+         GetSystemTimeAsFileTime(&ft);
+         li.LowPart = ft.dwLowDateTime;
+         li.HighPart = ft.dwHighDateTime;
+
+         uint64 ret = li.QuadPart;
+         ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
+         ret /= 10000; /* From 100 nano seconds (10^-7) to 1 millisecond (10^-3) intervals */
+
+         return ret;
+    #else
+         /* Linux */
+         struct timeval tv;
+
+         gettimeofday(&tv, NULL);
+
+         unsigned int ret = tv.tv_usec;
+         /* Convert from micro seconds (10^-6) to milliseconds (10^-3) */
+         ret /= 1000;
+
+         /* Adds the seconds (10^0) after converting them to milliseconds (10^-3) */
+         ret += (tv.tv_sec * 1000);
+
+         return ret;
+    #endif
 }
 
 /* --- getters / setters --- */
